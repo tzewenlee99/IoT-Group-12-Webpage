745e2f73edcaa9f0f1c5c6801defaf73342ee19a
danj <dan@theugroup.co>
2021-10-20 15:24:50 +0800

    flask front-end update 2

--- myproject/app.py
+++ myproject/app.py
@@ -8,7 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     data = db_init()
-    return render_template('main.html',
+    return render_template('index.html',
+                           latest_data=list(data['test'].values())[-1:],
+                           graph_data=list(data['test'].values())[-10:],
+                           img_data=list(data['images'].values())[-1:])
+@app.route("/images")
+def test():
+    data = db_init()
+    return render_template('image.html',
                            latest_data=list(data['test'].values())[-1:],
                            graph_data=list(data['test'].values())[-10:],
                            img_data=list(data['images'].values())[-1:])
